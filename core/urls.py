a27d28dbc369731ec3ad61705430aa61ed9b1b52
SanEk <tt.worm524@gmail.com>
2026-09-07 18:22:08 +0300

    transfer Web/login to core

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
-    path('register/', views.user_register, name='register'),
+    path('registration/', views.user_register, name='registration'),
     path('profile/', views.user_profile, name='profile'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import UserLoginForm, UserRegistrationForm, ProfileForm
 from .models import Profile
@@ -50,6 +50,12 @@ def user_register(request):
         form = UserRegistrationForm()
 
     return render(request, 'core/registration.html', {'form': form})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    messages.info(request, 'Вы успешно вышли из системы.')
+    return redirect('core:index')
 
 @login_required
 def user_profile(request):
