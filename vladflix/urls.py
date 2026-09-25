471e2829a082a152a64fbe449c7ede3ce59b9809
Artem Maltsev <artemmaltsevwork@gmail.com>
2026-09-25 21:13:13 +0300

    Style fixed when DEBUG = False

--- vladflix/urls.py
+++ vladflix/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.contrib.staticfiles.views import serve as staticfiles_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,8 +19,8 @@ urlpatterns += [
     ),
     re_path(
         r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')),
-        serve,
-        {'document_root': settings.STATICFILES_DIRS[0]},
+        staticfiles_serve,
+        {'insecure': True},
     ),
 ]
 
